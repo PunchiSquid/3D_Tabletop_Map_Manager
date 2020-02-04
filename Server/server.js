03ef22ec79caa36064fa19dd5921d1ed31f2db2a
PunchiSquid <50837462+PunchiSquid@users.noreply.github.com>
2020-02-04 17:36:04 +0000

    Set up initial Node and Heroku application
    Created basic server file that responds to GET requests by sending "Hello World".
    Created a Procfile to determine a starting command for Heroku hosting.
    Generated a package.json file for npm details and dependencies.
    Installed ExpressJS using npm, adding it as a dependency.

---
+++ Server/server.js
@@ -0,0 +1,26 @@
+// Node package require statements
+var express = require("express");
+var http = require("http");
+
+// Port used to listen for connections 
+// If hosted on Heroku, the port is defined by the service
+// If hosted locally the port is 9000
+const port = process.env.PORT || 9000;
+
+// Initialise the Express app.
+app = express();
+
+// Create HTTP server.
+server = http.createServer(app);
+
+// Default route
+app.get("/", function(request, response)
+{
+	response.send("Hello World");
+});
+
+// Listen for connections
+server.listen(port, function()
+{
+	console.log("Listening on " + port);
+});
