7bb3757481872db6a9801febe8ee34d800d4a334
PunchiSquid <50837462+PunchiSquid@users.noreply.github.com>
2020-02-12 13:48:16 +0000

    Added the GetUserAccountByUsername function to databaseFunctions.js
    Added GetUserAccountByUsername. The function retrieves the username and _id fields of a user_accounts record retrieved by a query that uses the username.

--- Server/databaseFunctions.js
+++ Server/databaseFunctions.js
@@ -17,6 +17,51 @@ module.exports = class MongoConnection
 	}
 	
 	/*
+	* Retrieves a user account by the username input.
+	* @Param inputUsername The username to find an account by.
+	*/
+	GetUserAccountByUsername(inputUsername)
+	{
+		// Create a local variable for the URL. The Promise seems to be unable to access the properties of the class.
+		let promiseURL = this.url;
+		
+		// Create and return a new promise for a later retrieved value
+		return new Promise(function(resolve, reject)
+		{				
+			// Set up a DB event handler for connections
+			MongoClient.connect(promiseURL,{ useNewUrlParser: true, useUnifiedTopology: true }).then(function(dbResponse)
+			{
+				// The specific database to access
+				var dbObject = dbResponse.db("map_manager_db");
+				
+				// Create a query based on input parameters
+				let query = { username: inputUsername };
+				let projection = {projection: { "password": 0, "emailAddress": 0} }
+				
+				// Finds and returns a single entry that matches the query
+				dbObject.collection("user_accounts").findOne(query, projection).then(function(response)
+				{
+					dbResponse.close()
+					resolve(response);
+					return;
+				})
+				.catch(function (error) 
+				{
+					// Close the connection and reject the promise and return the error
+					dbResponse.close();
+					reject(error);
+					return;
+				});
+			})
+			.catch(function(error)
+			{
+				reject(error);
+				return;
+			});	
+		});
+	}
+	
+	/*
 	* Inserts a single user account record to the database.
 	* @Param inputData The user account object to insert into the database.
 	*/
