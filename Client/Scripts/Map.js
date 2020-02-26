2d49e82c717c5850e425f60ad613b26cc42be72d
PunchiSquid <50837462+PunchiSquid@users.noreply.github.com>
2020-02-26 23:46:29 +0000

    Added the GenerateHeightMap method to the Map class
    GenerateHeightMap generates a new matrix of mapXDimension * mapYDimension dimensions, filling it with 1's to create a flat map of grid height.

--- Client/Scripts/Map.js
+++ Client/Scripts/Map.js
@@ -4,5 +4,23 @@ class Map
 	{
 		this.mapXDimension = mapXDimension;
 		this.mapYDimension = mapYDimension;
+
+		this.GenerateHeightMap();
+	}
+
+	/*
+	* Generates a flat height map matrix.
+	*/
+	GenerateHeightMap()
+	{
+		this.heightMap = new Array(this.mapXDimension);
+
+		for (let i = 0; i < this.heightMap.length; i++)
+		{
+			let column = new Array(this.mapYDimension);
+			column.fill(1);							// Fill with 1s to provide a flat plane
+			this.heightMap[i] = column;
+		}
+	}
 	}
 }
