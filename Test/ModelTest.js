f26894a6fe0789ff2f0709f55a5fad55c8c4a766
PunchiSquid <50837462+PunchiSquid@users.noreply.github.com>
2020-05-01 11:40:32 +0100

    Added unit tests for the HiddenRegion class
    Added tests for all accessors and constructors in the HiddenRegion class.
    Installed the threejs node package for testing purposes.

--- Test/ModelTest.js
+++ Test/ModelTest.js
@@ -1,8 +1,10 @@
 // Assertion library
 const assert = require('chai').assert;
+const THREE = require('three');
 
 // Model classes to test
 const Character = require('./../Client/Scripts/Character');
+const HiddenRegion = require('./../Client/Scripts/HiddenRegion');
 
 suite("Testing Character Model Functions", function()
 {
@@ -15,6 +17,11 @@ suite("Testing Character Model Functions", function()
 	setup(function()
 	{
 		testCharacter = new Character(ownerName);
+	});
+
+	teardown(function()
+	{
+		testCharacter = null;
 	});
 
 	test("Testing that character construction is correct", function()
@@ -67,5 +74,117 @@ suite("Testing Character Model Functions", function()
 
 		assert.isString(returnedValue);
 		assert.equal(returnedValue, ownerName, "Value should be equal to TestUser.");
+	});
+});
+
+suite("Testing HiddenRegion Model Functions", function()
+{
+	var testName = "testRegion";
+	var testRegion;
+
+	setup(function()
+	{
+		testRegion = new HiddenRegion(testName);
+	});
+
+	teardown(function()
+	{
+		testRegion = null;
+	});
+
+	test("Testing that hiddenRegion construction is correct", function()
+	{
+		assert.isString(testRegion.name);
+		assert.equal(testRegion.name, testName, "Value should be equal to TestRegion.");
+
+		assert.isArray(testRegion.hiddenBlocks);
+		assert.lengthOf(testRegion.hiddenBlocks, 0, "Array should be empty");
+
+		assert.isBoolean(testRegion.isHidden);
+		assert.isTrue(testRegion.isHidden, "Value should be true.");
+	});
+
+	test("Testing adding a single hidden block", function()
+	{
+		testRegion.AddHiddenBlock(1, 2);
+
+		let returnedArray = testRegion.GetHiddenBlocks();
+		assert.lengthOf(returnedArray, 1, "Array should have length 1");
+		assert.equal(returnedArray[0].x, 1, "X value should be 1");
+		assert.equal(returnedArray[0].y, 2, "Y value should by 2");
+	});
+
+	test("Testing adding multiple hidden blocks", function()
+	{
+		testRegion.AddHiddenBlock(1, 2);
+		testRegion.AddHiddenBlock(3, 4);
+
+		let returnedArray = testRegion.GetHiddenBlocks();
+		assert.lengthOf(returnedArray, 2, "Array should have length 2");
+		assert.equal(returnedArray[0].x, 1, "X value should be 1");
+		assert.equal(returnedArray[0].y, 2, "Y value should by 2");
+		assert.equal(returnedArray[1].x, 3, "X value should be 3");
+		assert.equal(returnedArray[1].y, 4, "Y value should by 4");
+	});
+
+	test("Testing removing a single hidden block", function()
+	{
+		testRegion.AddHiddenBlock(1, 2);
+		testRegion.AddHiddenBlock(3, 4);
+
+		testRegion.RemoveHiddenBlock(1, 2);
+
+		let returnedArray = testRegion.GetHiddenBlocks();
+		assert.lengthOf(returnedArray, 1, "Array should have length 1");
+		assert.equal(returnedArray[0].x, 3, "X value should be 3");
+		assert.equal(returnedArray[0].y, 4, "Y value should by 4");
+	})
+
+	test("Testing removing multiple hidden blocks.", function()
+	{
+		testRegion.AddHiddenBlock(1, 2);
+		testRegion.AddHiddenBlock(3, 4);
+		testRegion.AddHiddenBlock(5, 6);
+		testRegion.AddHiddenBlock(7, 8);
+
+		testRegion.RemoveHiddenBlock(1, 2);
+		testRegion.RemoveHiddenBlock(5, 6);
+
+		let returnedArray = testRegion.GetHiddenBlocks();
+		assert.lengthOf(returnedArray, 2, "Array should have length 2");
+		assert.equal(returnedArray[0].x, 3, "X value should be 3");
+		assert.equal(returnedArray[0].y, 4, "Y value should by 4");
+		assert.equal(returnedArray[1].x, 7, "X value should be 7");
+		assert.equal(returnedArray[1].y, 8, "Y value should by 8");
+	});
+
+	test("Testing getting the hidden blocks array", function()
+	{
+		testRegion.AddHiddenBlock(1, 2);
+		testRegion.AddHiddenBlock(3, 4);
+
+		let returnedArray = testRegion.GetHiddenBlocks();
+		assert.lengthOf(returnedArray, 2, "Array should have length 2");
+		assert.isArray(returnedArray);
+		assert.equal(returnedArray[0].x, 1, "X value should be 1");
+		assert.equal(returnedArray[0].y, 2, "Y value should by 2");
+		assert.equal(returnedArray[1].x, 3, "X value should be 3");
+		assert.equal(returnedArray[1].y, 4, "Y value should by 4");
+	});
+
+	test("Testing getting the isHidden property", function()
+	{
+		let returnedValue = testRegion.GetIsHidden();
+		assert.isBoolean(returnedValue);
+		assert.isTrue(returnedValue, "Value should be true.");
+	});
+
+	test("Testing setting the isHidden property", function()
+	{
+		testRegion.SetIsHidden(false);
+
+		let returnedValue = testRegion.GetIsHidden();
+		assert.isBoolean(returnedValue);
+		assert.isFalse(returnedValue, "Value should be false.");
 	});
 });
