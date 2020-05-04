98d230f1f6bbd8021c1dc56bc67e7323d89cab13
PunchiSquid <50837462+PunchiSquid@users.noreply.github.com>
2020-05-04 19:06:09 +0100

    Wrote unit tests for map functionality on the database

--- Test/DatabaseTest.js
+++ Test/DatabaseTest.js
@@ -7,6 +7,11 @@ const fs = require('fs');
 // Necessary custom modules
 const MongoConnection = require('./../Server/databaseFunctions');
 
+// Model classes for inserting into the database
+const Character = require('./../Client/Scripts/Character');
+const HiddenRegion = require('./../Client/Scripts/HiddenRegion');
+const Map = require('./../Client/Scripts/Map');
+
 // Load connection strings
 let rawdata = fs.readFileSync(__dirname + './../Server/connection.json');
 let connectionValues = JSON.parse(rawdata);
@@ -96,4 +101,123 @@ suite("Testing user account functions", function()
             done();
         });
     });
+});
+
+suite("Testing Map functions", function()
+{
+    let connection;
+	let accountId;
+	let mapId;
+	let retrievedMap;
+
+    suiteSetup(function(done)
+    {
+        connection = new MongoConnection(connectionValues.uri);
+
+        let testAccount = 
+        {
+            username: "TestUser",
+            password: "TestPassword",
+            emailAddress: "email@test.com",
+            mapRecords: new Array()
+        };
+
+        connection.AddUserAccount(testAccount).then(function(result)
+        {
+            accountId = result.insertedId;
+            done();
+        }); 
+    });
+
+    suiteTeardown(function(done)
+    {
+        connection.RemoveUserAccount(accountId).then(function(result)
+        {
+            connection = null;
+            done();
+        });
+    });
+
+	test("Testing the AddMapRecord function", function(done)
+	{
+		const map = new Map();
+		map.GenerateNewMap(125, 125);
+		map.name = "Test Name";
+		map.description = "Test Description";
+
+		connection.AddMapRecord(map, accountId).then(function(result)
+		{
+			assert.equal(result.insertedCount, 1, "Value of insertedCount should be 1");
+			mapId = result.insertedId;
+			done();
+		})
+		.catch(function(rejection)
+		{
+			assert.fail("Request rejected");
+			done();
+		});
+	});
+
+	test("Testing the GetMapRecord function", function(done)
+	{
+		connection.GetMapRecord(mapId).then(function(result)
+		{
+			retrievedMap = result;
+			assert.equal(String(result._id), String(mapId), "Values of the id fields should be equal");
+			assert.equal(result.name, "Test Name", "Value should be equal to Test Name");
+			assert.equal(result.description, "Test Description", "Value should be equal to Test Description");
+			done();
+		})
+		.catch(function(done)
+		{
+			assert.fail("Request rejected");
+			done();
+		})
+	});
+
+	test("Testing the GetMapRecords function", function(done)
+	{
+		const map = new Map();
+		map.GenerateNewMap(125, 125);
+		map.name = "Test Name";
+		map.description = "Test Description";
+
+		connection.AddMapRecord(map, accountId).then(function(result)
+		{
+			let secondMapId = result.insertedId;
+			
+			connection.GetMapRecords(accountId).then(function(result)
+			{
+				assert.equal(String(result[0]._id), String(mapId), "Value of map ids should match");
+				assert.equal(String(result[1]._id), String(secondMapId), "Value of map ids should match");
+				done();
+			})
+			.catch(function(done)
+			{
+				assert.fail("Request rejected");
+				done();
+			});
+		})
+		.catch(function(rejection)
+		{
+			assert.fail("Request rejected");
+			done();
+		});
+	});
+
+	test("Testing the UpdateMapRecord function", function(done)
+	{
+		retrievedMap.name = "New Name";
+
+		connection.UpdateMapRecord(retrievedMap).then(function(result)
+		{
+			assert.equal(result.modifiedCount, 1, "Value of modifiedCount should be 1");
+			done();
+		})
+		.catch(function(done)
+		{
+			assert.fail("Request rejected");
+			done();
+		});
+	});
 });
